0f06b6be665a5235bfba211416ec2ee1bdc270d2
unknown <xelton207@gmail.com>
2021-10-14 15:06:48 -0300

    :wink: primeiros passos no React :wink:

---
+++ meusite/src/App.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+const Equipe = (props) => {
+    return(
+        <div>
+          <Sobre nome={props.nome} cargo={props.cargo} idade=   {props.idade}/>
+
+          <Social fb={props.facebook}/>
+          <hr/>
+        </div>
+    )
+}
+
+const Sobre = (props) => {
+    return(
+        <div>
+            <h2>Olá sou o(a) {props.nome}</h2>
+            <h3>Cargo: {props.cargo}</h3>
+            <h4>Idade: {props.idade} anos</h4>
+        </div>
+    );
+}
+
+const Social = (props) => {
+    return(
+        <div>
+            <a href={props.fb}>facebook</a>
+        </div>
+    )
+}
+
+function App(){
+    return(
+        <div>
+            <h1>Conheça nossa equipe:</h1>
+            <Equipe nome="Elton" cargo="Programador" idade="23" facebook="https://google.com"/>
+            <Equipe nome="Everton" cargo="Musicista" idade="27"/>
+            <Equipe nome="Evelyn" cargo="Advogada" idade="21"/>
+
+        </div>
+    )
+}
+
+export default App;
