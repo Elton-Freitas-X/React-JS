1bbaace07a0d1c53cacbb886190753e3b6aaef07
unknown <xelton207@gmail.com>
2021-10-19 05:10:54 -0300

    :wink: trabalhando com listas em React

--- Usando-React/src/TrabListas.js
+++ Usando-React/src/TrabListas.js
@@ -1,10 +1,16 @@
 import React, { Component } from "react";
+import Feed from "./componentsList/Feed";
 
 class TrabListas extends Component{
     constructor(props){
         super(props)
         this.state = {
-
+            feed:[
+                {id: 1, username: 'Elton', curtidas:1, comentarios: 0},
+                {id: 2, username: 'Everton', curtidas:110, comentarios:24},
+                {id: 3, username: 'Evelyn', curtidas:1110, comentarios:224},
+                {id: 4, username: 'Lêda', curtidas:11120, comentarios:22224},
+            ]
         };
     }
 
@@ -12,7 +18,14 @@ class TrabListas extends Component{
     render(){
         return(
             <div>
-                <h1>Trabalhando com listas</h1>
+                
+                {this.state.feed.map((item)=>{
+                    return(
+                        <Feed key={item.id} username={item.username}
+                        curtidas={item.curtidas} comentarios={item.comentarios}/>
+                    );
+                })}
+
             </div>
         )
     }
